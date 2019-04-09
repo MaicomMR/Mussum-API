82b77f957ea139fd3d996c2782d07bafd5b51b53
Eduardo Rib <eduardoxrib@gmail.com>
2019-04-08 22:44:54 -0300

    Adicionando arquivo para popular novas unidades curriculares

---
+++ src/main/resources/db/migration/V011__Popular_Tabela_Unidades_II.sql
@@ -0,0 +1,16 @@
+INSERT INTO unidade
+    (ativo, nome)
+    VALUES
+    (1, "Algoritmos e Programação I");
+INSERT INTO unidade
+    (ativo, nome)
+    VALUES
+    (1, "User Experience");
+INSERT INTO unidade
+    (ativo, nome)
+    VALUES
+    (1, "Lógica de Programação");
+INSERT INTO unidade
+    (ativo, nome)
+    VALUES
+    (1, "Matemática Aplicada");
